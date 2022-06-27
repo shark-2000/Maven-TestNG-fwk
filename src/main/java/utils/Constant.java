7bd175fe14df91ba97e429e3e89c1e3a6ba0a27e
shark-2000 <katyayns40@gmail.com>
2022-06-27 15:28:16 +0530

    adding edge linux driver

--- src/main/java/utils/Constant.java
+++ src/main/java/utils/Constant.java
@@ -4,6 +4,6 @@ public class Constant {
 	public static String ChromeDriverKey = "webdriver.chrome.driver";
 	public static String ChromeDriverPath = System.getProperty("user.dir")+"/src/test/resources/drivers/chromedriver";
 	public static String EdgeDriverKey = "webdriver.edge.driver";
-	public static String EdgeDriverPath = System.getProperty("user.dir")+"/src/test/resources/drivers/msedgedriver.exe";
+	public static String EdgeDriverPath = System.getProperty("user.dir")+"/src/test/resources/drivers/msedgedriver";
 
 }
